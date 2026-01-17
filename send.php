e639d201ec07cbc92b287cfd7bf33f8ee8b49894
Viktoria Zaharova <zaharova181114@gmail.com>
2026-01-18 00:28:11 +0200

    edit 1

--- send.php
+++ send.php
@@ -1,10 +1,23 @@
 <?php
-if($_POST){
+if ($_POST) {
+
     $to = "WEBSOFT.BEST@gmail.com";
-    $subject = "Новая заявка с сайта";
-    $message = "Имя: ".$_POST['name']."\nEmail: ".$_POST['email']."\nСообщение: ".$_POST['message'];
-    $headers = "From: ".$_POST['email'];
-    mail($to,$subject,$message,$headers);
+    $subject = "Новая заявка с сайта WebSoft.Best";
+
+    $name = strip_tags($_POST['name']);
+    $email = strip_tags($_POST['email']);
+    $messageText = strip_tags($_POST['message']);
+
+    $message = "Имя: $name\n";
+    $message .= "Email: $email\n\n";
+    $message .= "Сообщение:\n$messageText";
+
+    $headers = "From: Website <no-reply@yourdomain.com>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    mail($to, $subject, $message, $headers);
+
     echo "Спасибо! Мы свяжемся с вами.";
 }
 ?>
